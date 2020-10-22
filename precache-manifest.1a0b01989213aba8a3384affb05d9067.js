0e19611a7f0eeac793c1f106826c182e57ab48a9
helderferrari2 <helderferrari2@gmail.com>
2020-10-21 21:35:19 -0300

    Updates

---
+++ precache-manifest.1a0b01989213aba8a3384affb05d9067.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35efaccf510f583fd4c9ecc089d237a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b59bc820fc85d2300de5",
+    "url": "/pokedex/static/css/2.842e1da4.chunk.css"
+  },
+  {
+    "revision": "befabf0277987eafd98d",
+    "url": "/pokedex/static/css/main.18607980.chunk.css"
+  },
+  {
+    "revision": "b59bc820fc85d2300de5",
+    "url": "/pokedex/static/js/2.14f185d5.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/pokedex/static/js/2.14f185d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befabf0277987eafd98d",
+    "url": "/pokedex/static/js/main.fea72cd1.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "bb649d6eb4056cb8b388ec46668e1234",
+    "url": "/pokedex/static/media/logo.bb649d6e.png"
+  },
+  {
+    "revision": "47d1fa89e28b0693b8548c188f88db75",
+    "url": "/pokedex/static/media/pokeball.47d1fa89.svg"
+  }
+]);
